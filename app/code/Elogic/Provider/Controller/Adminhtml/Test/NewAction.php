6d3e7331ab257aa89cc5b545af91faf21172c8e7
bRakochyi <b.rakochyi@gmail.com>
2021-07-02 09:42:08 +0300

    branch 4

--- app/code/Elogic/Provider/Controller/Adminhtml/Test/NewAction.php
+++ app/code/Elogic/Provider/Controller/Adminhtml/Test/NewAction.php
@@ -3,6 +3,8 @@
 namespace Elogic\Provider\Controller\Adminhtml\Test;
 
 use Elogic\Provider\Model\Provider;
+use Magento\Backend\App\Action\Context;
+use Elogic\Provider\Model\ProviderFactory;
 
 /**
  * Class NewAction
@@ -10,6 +12,21 @@ use Elogic\Provider\Model\Provider;
  */
 class NewAction extends \Magento\Backend\App\Action
 {
+    /**
+     * @var \Elogic\Provider\Model\ProviderFactory
+     */
+    protected $providerFactory;
+
+
+
+    public function __construct(
+        Context $context,
+        \Elogic\Provider\Model\ProviderFactory $providerFactory
+    )
+    {
+        $this->providerFactory = $providerFactory;
+        parent::__construct($context);
+    }
 
     /**
      * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\Controller\ResultInterface
@@ -22,7 +39,7 @@ class NewAction extends \Magento\Backend\App\Action
 
         $providerData = $this->getRequest()->getParam('provider');
         if (is_array($providerData)) {
-            $provider = $this->_objectManager->create(Provider::class);
+            $provider = $this->providerFactory->create(Provider::class);
             $provider->setData($providerData)->save();
             $resultRedirect = $this->resultRedirectFactory->create();
             return $resultRedirect->setPath('*/*/index');
