40995e621e2b933e39b63cd9d055ec61ada8d470
bRakochyi <b.rakochyi@gmail.com>
2021-07-05 11:15:33 +0300

    branch 5

---
+++ app/code/Elogic/Provider/Controller/Adminhtml/Test/Provider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Elogic\Provider\Controller\Adminhtml\Test;
+
+/**
+ * Class Provider
+ * @package Elogic\Provider\Controller\Adminhtml\Test
+ */
+abstract class Provider extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Elogic_Provider::Provider';
+}
